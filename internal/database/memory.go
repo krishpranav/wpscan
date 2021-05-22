fcd48fa6725b273d4c9bea1f04d46e513379186f
krishpranav <krisna.pranav@gmail.com>
2021-05-22 16:30:31 +0530

    [+] Added new memory functions [+]

--- internal/database/memory.go
+++ internal/database/memory.go
@@ -16,3 +16,18 @@ type memory struct {
 	boolx     map[string]bool
 	mapstring map[string]map[string]string
 }
+
+func NewMemory() *memory {
+	database := &memory{
+		stringx:   map[string]string{},
+		intx:      map[string]int{},
+		boolx:     map[string]bool{},
+		slice:     map[string][]string{},
+		mapstring: map[string]map[string]string{},
+	}
+
+	database.mapstring["HTTP Plugins Versions"] = map[string]string{}
+	database.mapstring["HTTP Themes Versions"] = map[string]string{}
+
+	return database
+}
