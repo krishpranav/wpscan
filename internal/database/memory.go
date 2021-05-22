80d61a4ddb6be3fe4357e8161e9a4e03a7ba889f
krishpranav <krisna.pranav@gmail.com>
2021-05-22 16:38:30 +0530

    [+] added some strings [+]

--- internal/database/memory.go
+++ internal/database/memory.go
@@ -1,8 +1,6 @@
 package database
 
-import (
-	"sync"
-)
+import "sync"
 
 var (
 	Memory = NewMemory()
@@ -37,33 +35,49 @@ func (db *memory) SetString(key, value string) {
 	db.stringx[key] = value
 	mutex.Unlock()
 }
-
 func (db *memory) SetSlice(key string, value []string) {
 	mutex.Lock()
 	db.slice[key] = value
 	mutex.Unlock()
 }
-
 func (db *memory) SetInt(key string, value int) {
 	mutex.Lock()
 	db.intx[key] = value
 	mutex.Unlock()
 }
-
 func (db *memory) SetBool(key string, value bool) {
 	mutex.Lock()
 	db.boolx[key] = value
 	mutex.Unlock()
 }
-
 func (db *memory) SetMapString(key string, value map[string]string) {
 	mutex.Lock()
 	db.mapstring[key] = value
 	mutex.Unlock()
 }
-
 func (db *memory) SetMapMapString(key, key2, value string) {
 	mutex.Lock()
 	db.mapstring[key][key2] = value
+	mutex.Unlock()
+}
+
+func (db *memory) AddInString(key, value string) {
+	mutex.Lock()
+	db.stringx[key] += value
+	mutex.Unlock()
+}
+func (db *memory) AddInSlice(key, value string) {
+	mutex.Lock()
+	db.slice[key] = append(db.slice[key], value)
+	mutex.Unlock()
+}
+func (db *memory) AddCalcInt(key string, value int) {
+	mutex.Lock()
+	db.intx[key] = db.intx[key] + value
+	mutex.Unlock()
+}
+func (db *memory) AddInt(key string) {
+	mutex.Lock()
+	db.intx[key]++
 	mutex.Unlock()
 }
