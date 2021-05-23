057454e778071fa5e9e0169cf65fd54b02a45fdd
krishpranav <krisna.pranav@gmail.com>
2021-05-23 11:26:39 +0530

    [+] Added http options [+]

---
+++ pkg/gohttp/gohttp.go
@@ -0,0 +1,26 @@
+package gohttp
+
+import (
+	"io"
+	"net/http"
+	"net/url"
+	"time"
+)
+
+var (
+	firewallPassing bool
+)
+
+type httpOptions struct {
+	url                  *URLOptions
+	method               string
+	tlsCertificateVerify bool
+	tor                  bool
+	proxy                func(*http.Request) (*url.URL, error)
+	data                 io.Reader
+	userAgent            string
+	redirect             func(req *http.Request, via []*http.Request) error
+	contentType          string
+	firewall             bool
+	sleep                time.Duration
+}
