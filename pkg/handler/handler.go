776105e3b34c6472ada08ecd2892efffd17fdf37
krishpranav <krisna.pranav@gmail.com>
2021-05-23 12:18:38 +0530

    [+] handler [+]

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -21,8 +21,32 @@ func HandlerErrorTorProxy() {
 
 func HandlerErrorURL() {
 	if recovered := recover(); recovered != nil {
-		recoverdS := fmt.Sprintf("%s", recovered)
+		recoveredS := fmt.Sprintf("%s", recovered)
 
-		printer.Fatal(recoverdS + "\n")
+		printer.Fatal(recoveredS + "\n")
+	}
+}
+
+func HandlerErrorGetVuln() {
+	if recovered := recover(); recovered != nil {
+		recoveredS := fmt.Sprintf("%s", recovered)
+
+		if strings.Contains(recoveredS, "dial tcp 144.217.235.104:8777: connect: connection refused") {
+			printer.Danger("Connection refused to API").L()
+		} else {
+			printer.Danger(recoveredS).L()
+		}
+	}
+}
+
+func HandlerErrorLuaScripts() {
+	if recovered := recover(); recovered != nil {
+		recoveredS := fmt.Sprintf("%s", recovered)
+
+		if strings.Contains(recoveredS, "This script does not exist.") {
+			printer.Fatal(recoveredS + "\n")
+		} else {
+			printer.Danger(recoveredS).L()
+		}
 	}
 }
