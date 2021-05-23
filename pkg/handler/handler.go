8fe22f529f42c0520e70e28fe23e358a970c8069
krishpranav <krisna.pranav@gmail.com>
2021-05-23 12:15:42 +0530

    [+] Handle Error Tor Proxy [+]

---
+++ pkg/handler/handler.go
@@ -0,0 +1,20 @@
+package handler
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/krishpranav/wpscan/pkg/printer"
+)
+
+func HandlerErrorTorProxy() {
+	if recovered := recover(); recovered != nil {
+		recoveredS := fmt.Sprintf("%s", recovered)
+
+		if strings.Contains(recoveredS, "proxyconnect tcp: dial tcp 127.0.0.1:9080: connect: connection refused") {
+			printer.Fatal("Connection Refused, the tor with the command: \"tor --HTTPTunnelPort 9080\"\n")
+		} else {
+			printer.Danger(recoveredS).L()
+		}
+	}
+}
