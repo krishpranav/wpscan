4a5abfba91cb54d016dccecfeeffe98ee8d429cd
krishpranav <krisna.pranav@gmail.com>
2021-05-24 08:47:54 +0530

    [+] added new scripts [+]

--- pkg/scripts/lua.go
+++ pkg/scripts/lua.go
@@ -16,3 +16,17 @@ type structscript struct {
 type script struct {
 	lstate *lua.LState
 }
+
+func NewScript() *script {
+	LuaNewState := lua.NewState()
+
+	LuaNewState.PreloadModule("url", luaUrl.Loader)
+	LuaNewState.PreloadModule("printer", luaPrinter.Loader)
+	LuaNewState.PreloadModule("net", luaNet.Loader)
+
+	luaLibs.Preload(LuaNewState)
+
+	LuaNewState.SetGlobal("tor_url", lua.LString("http://127.0.0.1:9080"))
+
+	return &script{lstate: LuaNewState}
+}
