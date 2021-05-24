e2c87d57fa9dfe4167a42eb8b49d7cf1c1c5c180
krishpranav <krisna.pranav@gmail.com>
2021-05-24 08:55:42 +0530

    [+] printer [+]

--- pkg/scripts/lib/net/net.go
+++ pkg/scripts/lib/net/net.go
@@ -1,6 +1,10 @@
 package net
 
 import (
+	"fmt"
+	"net"
+
+	"github.com/krishpranav/wpscan/pkg/printer"
 	lua "github.com/yuin/gopher-lua"
 )
 
@@ -8,5 +12,19 @@ func Loader(L *lua.LState) int {
 	mod := L.SetFuncs(L.NewTable(), exports)
 
 	L.Push(mod)
+	return 1
+}
+
+func lookupip(L *lua.LState) int {
+	ips, err := net.LookupIP(L.ToString(1))
+
+	if err != nil {
+		printer.Danger(fmt.Sprintf("%s", err))
+	}
+
+	ip := fmt.Sprintf("%s", ips[0])
+
+	L.Push(lua.LString(ip))
+
 	return 1
 }
