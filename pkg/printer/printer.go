63594320622ece1ed85ac04d19e5a1b7b5775db3
krishpranav <krisna.pranav@gmail.com>
2021-05-24 08:39:47 +0530

    [+] added variables [+]

--- pkg/printer/printer.go
+++ pkg/printer/printer.go
@@ -14,3 +14,19 @@ var (
 	zfill    = &z{}
 	Required = color.Red("(Required)").Bold().String()
 )
+
+var (
+	prefixDanger  = color.Red("[✗]").String()
+	prefixFatal   = color.Red("[!]").String()
+	prefixDone    = color.Green("[✔]").String()
+	prefixWarning = color.Yellow("[!]").String()
+	prefixScan    = color.Yellow("[?]").String()
+	prefixInfo    = color.Magenta("[i]").String()
+
+	prefixListDanger  = color.Red("    —").String()
+	prefixListDone    = color.Green("    —").String()
+	prefixListDefault = color.White("    —").String()
+	prefixListWarning = color.Yellow("    —").String()
+
+	prefixTopLine = color.Yellow("[✲]").String()
+)
