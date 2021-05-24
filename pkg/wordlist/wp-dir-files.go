e745f554317c4ec449ef1989c1ecc439d16379f7
krishpranav <krisna.pranav@gmail.com>
2021-05-24 10:41:42 +0530

    [+] wp dir files updated [+]

---
+++ pkg/wordlist/wp-dir-files.go
@@ -0,0 +1,63 @@
+package wordlist
+
+// WPreadme ::
+var WPreadme = [3]string{
+	"readme.txt",
+	"readme.md",
+	"readme",
+}
+
+// WPchangesLogs ::
+var WPchangesLogs = [3]string{
+	"changelog.txt",
+	"changelog.md",
+	"changelog",
+}
+
+var WPreleaseLog = [3]string{
+	"release_log.txt",
+	"release_log.md",
+	"release_log",
+}
+
+// WPfpd :: These are the files that can contain a "full path disclosure".
+var WPfpd = [...]string{
+	"404.php",
+	"archive.php",
+	"author.php",
+	"comments.php",
+	"footer.php",
+	"functions.php",
+	"header.php",
+	"image.php",
+	"page.php",
+	"search.php",
+	"single.php",
+	"archive.php",
+}
+
+// WPlicense ::
+var WPlicense = [3]string{
+	"license.txt",
+	"license.md",
+	"license",
+}
+
+// WPdirectorys ::
+var WPdirectorys = [...]string{
+	"js",
+	"css",
+	"images",
+	"inc",
+	"admin",
+	"src",
+	"widgets",
+	"lib",
+	"assets",
+	"includes",
+	"logs",
+	"vendor",
+	"core",
+	"wp-admin",
+	"wp-content",
+}
