fbf9437a7ce1927301cff3babc9d8a839530fe1a
krishpranav <krisna.pranav@gmail.com>
2021-05-24 08:48:34 +0530

    [+] added lua script converter [+]

--- pkg/scripts/lua.go
+++ pkg/scripts/lua.go
@@ -1,7 +1,23 @@
 package scripts
 
+import (
+	"path/filepath"
+	"strings"
+
+	"github.com/krishpranav/wpscan/internal/database"
+	"github.com/krishpranav/wpscan/pkg/handler"
+	luaNet "github.com/krishpranav/wpscan/pkg/scripts/lib/net"
+	luaPrinter "github.com/krishpranav/wpscan/pkg/scripts/lib/printer"
+	luaUrl "github.com/krishpranav/wpscan/pkg/scripts/lib/url"
+	"github.com/krishpranav/wpscan/pkg/text"
+	luaLibs "github.com/vadv/gopher-lua-libs"
+	"github.com/yuin/gluamapper"
+	lua "github.com/yuin/gopher-lua"
+)
+
+// PathScript:: this is directory scripts .lua
 var (
-	PathScript = "tools/scripts"
+	PathScript = "tools/scripts/"
 )
 
 type structscript struct {
@@ -29,4 +45,100 @@ func NewScript() *script {
 	LuaNewState.SetGlobal("tor_url", lua.LString("http://127.0.0.1:9080"))
 
 	return &script{lstate: LuaNewState}
+}
+
+func (s *script) UseScript(n string) (sttst *structscript) {
+	defer handler.HandlerErrorLuaScripts()
+
+	if _, has := text.ContainsSliceString(AllScripts(), n); has {
+		path := strings.ReplaceAll(n, ".lua", "")
+		path = PathScript + path + ".lua"
+
+		if err := s.lstate.DoFile(path); err != nil {
+			panic(err)
+		}
+
+		if err := gluamapper.Map(s.lstate.GetGlobal("script").(*lua.LTable), &sttst); err != nil {
+			panic(err)
+		}
+	} else {
+		panic("The " + n + " script does not exist")
+	}
+
+	return
+}
+
+func AllScripts() []string {
+	defer handler.HandlerErrorLuaScripts()
+
+	var list []string
+
+	scriptsfileslist, err := filepath.Glob(PathScript + "*.lua")
+
+	if err != nil {
+		panic(err)
+	}
+
+	for _, files := range scriptsfileslist {
+		files = strings.ReplaceAll(files, PathScript, "")
+		files = strings.ReplaceAll(files, ".lua", "")
+
+		list = append(list, files)
+	}
+
+	return list
+}
+
+func Exists(n string) bool {
+	if _, has := text.ContainsSliceString(AllScripts(), n); has {
+		return true
+	}
+
+	return false
+}
+
+func GetInfos(n string) (sttst *structscript) {
+	defer handler.HandlerErrorLuaScripts()
+
+	if _, has := text.ContainsSliceString(AllScripts(), n); has {
+		lstate := lua.NewState()
+
+		path := strings.ReplaceAll(n, ".lua", "")
+		path = PathScript + path + ".lua"
+
+		lstate.PreloadModule("url", luaUrl.Loader)
+		lstate.PreloadModule("printer", luaPrinter.Loader)
+		lstate.PreloadModule("net", luaNet.Loader)
+
+		luaLibs.Preload(lstate)
+
+		lstate.SetGlobal("tor_url", lua.LString("http://127.0.0.1:9080"))
+
+		if err := lstate.DoFile(path); err != nil {
+			panic(err)
+		}
+
+		if err := gluamapper.Map(lstate.GetGlobal("script").(*lua.LTable), &sttst); err != nil {
+			panic(err)
+		}
+	} else {
+		panic("The " + n + " script does not exist")
+	}
+
+	return
+}
+
+func (s *script) Run() {
+	defer s.lstate.Close()
+	defer handler.HandlerErrorLuaScripts()
+
+	err := s.lstate.CallByParam(lua.P{
+		Fn:      s.lstate.GetGlobal("main"),
+		NRet:    0,
+		Protect: true,
+	}, lua.LString(database.Memory.GetString("Target")))
+
+	if err != nil {
+		panic(err)
+	}
 }
